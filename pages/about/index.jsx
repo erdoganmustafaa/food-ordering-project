44e9c5e1f3698eebeb489126d53744d080bdefd0
Mustafa Erdoğan <43342664+erdoganmustafaa@users.noreply.github.com>
2022-12-22 17:34:36 +0300

    Update index.jsx

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -14,7 +14,7 @@ function Index() {
         <div className='sm:mx-auto mx-5'>
             <Title addClass="sm:text-[40px] text-[52px]">We are Feane</Title>
             <p className='my-6'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
             </p>
             <button className='btn-primary'>Read more</button>
         </div>
